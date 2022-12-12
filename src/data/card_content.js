73184dd4c0ab6489dfac44da7b514ce4057fe575
severo <oliveira.severo@gmail.com>
2022-12-12 11:00:05 -0300

    feat: :card_file_box: Base de carrossel com 3 itens

--- src/data/card_content.js
+++ src/data/card_content.js
@@ -18,16 +18,25 @@ const CarouselItems = [
       figure_caption: "Atletas",
       list: [
         {
-          item: "fdfdfdfdfdf"
+          item: "Para não haver grandes aglomerações, limitamos à QUINZE o número ciclistas por passeio;"
         },
+        {
+          item: "Durante o passeio manteremos distanciamento de segurança;"
+        },
+        {
+          item: "<strong>SERÁ OBRIGATÓRIO O USO DE MÁSCARA</strong> durante todo o percurso;"
+        },
+        {
+          item: "Contamos com a compreensão de todos. Vamos respeitar a indicação da OMS e fazer as atividades de forma segura e confortável."
+        }
       ]
     },
     {
       id: 3,
-      title: "O que levar", 
+      title: "Amet consectetur", 
       picture: Foto_03,
       figure_caption: "Atletas",
-      text: "Capacete, luvas, óculos, lanche básico, documentos, kit com ferramentas para pequenos reparos na bike, powerlink, câmera de ar, bomba, canivete pequeno, lanterna."
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia veniam provident odit doloremque accusantium cupiditate quo maiores maxime modi unde. Commodi pariatur eius architecto nobis ducimus. Molestiae quisquam fugiat ea!"
     },
 ]
 
