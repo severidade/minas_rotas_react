bb9ff4d558a8c5de51ffd72a2b14434802e560bb
severo <oliveira.severo@gmail.com>
2022-12-12 08:58:17 -0300

    feat: :card_file_box: criação do json para o carrrosel e da pasta com imagens

---
+++ src/data/card_content.js
@@ -0,0 +1,34 @@
+import Foto_01 from '../img/01.png';
+import Foto_02 from '../img/02.png';
+import Foto_03 from '../img/03.png';
+
+
+const CarouselItems = [
+    {
+      id: 1,
+      title: "O que levar", 
+      picture: Foto_01,
+      figure_caption: "Atletas",
+      text: "Capacete, luvas, óculos, lanche básico, documentos, kit com ferramentas para pequenos reparos na bike, powerlink, câmera de ar, bomba, canivete pequeno, lanterna."
+    },
+    {
+      id: 2,
+      title: "O que levar", 
+      picture: Foto_02,
+      figure_caption: "Atletas",
+      list: [
+        {
+          item: "fdfdfdfdfdf"
+        },
+      ]
+    },
+    {
+      id: 3,
+      title: "O que levar", 
+      picture: Foto_03,
+      figure_caption: "Atletas",
+      text: "Capacete, luvas, óculos, lanche básico, documentos, kit com ferramentas para pequenos reparos na bike, powerlink, câmera de ar, bomba, canivete pequeno, lanterna."
+    },
+]
+
+export default CarouselItems; 
