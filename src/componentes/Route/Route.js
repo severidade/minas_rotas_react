80c81e17bdf746d224453c3e201b7f9b2a0415e4
severo <oliveira.severo@gmail.com>
2023-02-15 10:02:16 -0300

    refactor: :recycle: Organizando CSS e estrutura da seção vídeos

--- src/componentes/Route/Route.js
+++ src/componentes/Route/Route.js
@@ -1,23 +1,27 @@
 import Content from '../../data/Content';
-
-import './Route.css'
+import styles from './Route.module.css';
 import ShareButton from '../ShareButton/ShareButton';
 
 const parse = require('html-react-parser');
 
 export default function Route() {
+  const {
+    container_route,
+    container_route_text,
+    container_map,
+    container_share,
+  } = styles
   return (
-
-    <div className='container_route'>
-      <div className='container_route_text'>
+    <div className={ container_route }>
+      <div className={ container_route_text }>
         <h2>{parse(Content.route.title)}</h2>
         <p>{parse(Content.route.text)}</p>
       </div>
-      <div className='container_map'>
+      <div className={container_map}>
         {parse(Content.route.map)}
       </div>
       <div
-        className='container_share'
+        className={container_share}
       >
         <h3>Compartilhe</h3>
         <ShareButton shareType={"whatsapp"} link={Content.route.share} />
